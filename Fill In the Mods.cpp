0395636a23ae4ffca0db3304cc189d7a977264bc
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-01-17 09:48:44 -0500

    Update Fill In the Mods.cpp

--- Fill In the Mods.cpp
+++ Fill In the Mods.cpp
@@ -6,12 +6,11 @@ using namespace std;
 
 int main()
 {
-    // 1. Set a equal to the remainder of 144 divided by 13
+    // 1. Change the next line so that a is set to the remainder of 144 divided by 13
     int a = 0;
-    /* your code here */
     cout << "1. The remainder of 144 divided by 13 is: " << a << endl << endl;
 
-
+    //***********************************************************************************
     // 2. Add an if/else to determine whether a number entered by the user is even or odd
     //    Even numbers are multiples of 2, so when divided by 2 they have remainder 0
     int b;
@@ -22,8 +21,9 @@ int main()
     cout << b << " is EVEN" << endl << endl;
 
     cout << b << " is ODD" << endl << endl;
-
-
+    
+    
+    //***********************************************************************************
     // 3. Add and if/else statement to determine whether a number entered 
     //    by the user is a multiple of both 3 and 5
     int c;
@@ -34,9 +34,18 @@ int main()
     cout << c << " is a multiple of 3 and 5" << endl << endl;
 
     cout << c << " is NOT a multiple of 3 and 5" << endl << endl;
-   
 
-    // 4. Add and if/else statement to determine whether a number entered 
+    //***********************************************************************************
+    // 4. The following code contains a loop that displays all the number from 1 to 100.
+    //    Add an if statement to the loop so that only the multiples of 10 are displayed
+    int number = 1;
+    while(number <= 100){
+       cout << number << endl;
+       number = number + 1;
+    }
+   
+    //***********************************************************************************
+    // 5. Add and if/else statement to determine whether a number entered 
     //    by the user is within 1 of a multiple of 10
     //    For example:  19, 20, 21 are within 1 of a multiple of 10
     //                 but 18 and 22 are more than 1 away from a multiple of 10
